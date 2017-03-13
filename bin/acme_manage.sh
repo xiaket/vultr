7000631c47c446da4f89be929185e4c03c20cabd
Kai Xia(夏恺) <xiaket@gmail.com>
2017-03-13 15:52:12 +1100

    add acme related scripts.
    Signed-off-by: Kai Xia(夏恺) <xiaket@gmail.com>

---
+++ bin/acme_manage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ACME='/data/var/lib/acme/acme.sh --home /data/var/lib/acme --config-home /data/var/lib/acme/data --cert-home /data/var/lib/acme/certs'
+
+# $ACME --issue --dns dns_gd -d torrent.xiaket.org
+# $ACME --issue --dns dns_gd -d blog.xiaket.org
+
+$ACME --install-cert -d blog.xiaket.org --keypath /data/etc/ssl/blog.xiaket.org.key --fullchainpath /data/etc/ssl/blog.xiaket.org.crt --reloadcmd  "service blog restart"
+$ACME --install-cert -d torrent.xiaket.org --keypath /data/etc/ssl/torrent.xiaket.org.key --fullchainpath /data/etc/ssl/torrent.xiaket.org.crt
