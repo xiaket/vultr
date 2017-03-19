62fb6deec9de09958628021ca57aa5b2076a2e54
Kai Xia(夏恺) <xiaket@gmail.com>
2017-03-20 09:56:31 +1100

    add acme management script.
    Signed-off-by: Kai Xia(夏恺) <xiaket@gmail.com>

--- bin/acme_manage.sh
+++ bin/acme_manage.sh
@@ -4,6 +4,8 @@ ACME='/data/var/lib/acme/acme.sh --home /data/var/lib/acme --config-home /data/v
 
 # $ACME --issue --dns dns_gd -d torrent.xiaket.org
 # $ACME --issue --dns dns_gd -d blog.xiaket.org
+# $ACME --issue --dns dns_gd -d d.xiaket.org
 
 $ACME --install-cert -d blog.xiaket.org --keypath /data/etc/ssl/blog.xiaket.org.key --fullchainpath /data/etc/ssl/blog.xiaket.org.crt --reloadcmd  "service blog restart"
-$ACME --install-cert -d torrent.xiaket.org --keypath /data/etc/ssl/torrent.xiaket.org.key --fullchainpath /data/etc/ssl/torrent.xiaket.org.crt
+$ACME --install-cert -d torrent.xiaket.org --keypath /data/etc/ssl/torrent.xiaket.org.key --fullchainpath /data/etc/ssl/torrent.xiaket.org.crt --reloadcmd  "service transmission restart"
+$ACME --install-cert -d d.xiaket.org --keypath /data/etc/ssl/d.xiaket.org.key --fullchainpath /data/etc/ssl/d.xiaket.org.crt --reloadcmd  "service d restart"
